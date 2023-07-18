e125eba368afe67ef30d4c34f03790a44fddafbc
wleite <wleite@student.42sp.org.br>
2023-07-18 11:12:34 -0300

    feat: add OpenAI integration

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
+import os
 
 # Create your views here.
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    answer = response.choices[0].text.strip()
+    return answer
 
 
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Hi this is my response'
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
