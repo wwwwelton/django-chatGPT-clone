484a71aa52324786ab9e46cf26297dbd09bc04c7
wleite <wleite@student.42sp.org.br>
2023-07-19 10:23:41 -0300

    feat: add login/logout

--- chatbot/views.py
+++ chatbot/views.py
@@ -33,7 +33,19 @@ def chatbot(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html',
+                          {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 
 
 def register(request):
@@ -62,3 +74,4 @@ def register(request):
 
 def logout(request):
     auth.logout(request)
+    return redirect('login')
