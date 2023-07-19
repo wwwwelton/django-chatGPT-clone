a05b3fb7e60c404db48410428df239a6c7ba884d
wleite <wleite@student.42sp.org.br>
2023-07-19 10:05:14 -0300

    feat: add register view and template

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
 import os
+
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -27,3 +30,35 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'register.html',
+                              {'error_message': error_message})
+        else:
+            error_message = 'Password dont match'
+            return render(request, 'register.html',
+                          {'error_message': error_message})
+    return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
