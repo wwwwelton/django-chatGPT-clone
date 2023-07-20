455855841d5abe416b1adc9bfc155923c79816e2
wleite <wleite@student.42sp.org.br>
2023-07-20 10:17:41 -0300

    feat: save chat history to database

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,6 +5,9 @@ import os
 
 from django.contrib import auth
 from django.contrib.auth.models import User
+from .models import Chat
+
+from django.utils import timezone
 
 # Create your views here.
 
@@ -28,6 +31,10 @@ def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
+
+        chat = Chat(user=request.user, message=message,
+                    response=response, created_at=timezone.now())
+        chat.save()
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
 
